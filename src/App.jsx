ac612cd0d01adaee6889ce506606069f31c44f58
Rana Pratap Singh Patel <ranapsp317@gmail.com>
2024-05-27 20:00:49 +0530

    updated message

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='w-full h-full flex flex-col background relative overfow-x-hidden items-center'>
      <div className='bg-white rounded-lg w-11/12 text-center mt-[40px]
-      px-10 py-2 text-4xl font-bold'>RANDOM GIF</div>
+      px-10 py-2 text-4xl font-bold'>RANDOM GIF of limit 100/hour</div>
      <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
       <Random/>
       <Tag/>
